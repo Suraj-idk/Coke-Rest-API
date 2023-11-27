1621711215e6f0589a245ac4e2753bcad4484427
jadhavsuraj27 <surajjadhav022@gmail.com>
2023-11-27 20:49:26 +0530

    Making the cron job run every 15min and update status accordingly

--- src/main/java/com/example/CokeRestAPI/Service/SheetsService.java
+++ src/main/java/com/example/CokeRestAPI/Service/SheetsService.java
@@ -62,140 +62,6 @@ public class SheetsService {
         System.out.println("Data written to sheet: " + result);
     }
 
-    public void addOrderStatus(Map<String, Object> rowData) {
-        String orderDateStr = (String) rowData.get("Ordered Date");
-        String deliveryDateStr = (String) rowData.get("Est. Delivery Date");
-
-        if (orderDateStr != null && deliveryDateStr != null) {
-            try {
-                // Parse date strings into Date objects
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                Date orderDate = dateFormat.parse(orderDateStr);
-                Date deliveryDate = dateFormat.parse(deliveryDateStr);
-
-                // Get today's date
-                Date today = new Date();
-
-                // Determine the order status based on the logic
-                String orderStatus = "";
-
-                if (isSameDay(today, orderDate)) {
-                    orderStatus = "Preparing for Dispatch";
-                } else if (today.before(addDays(orderDate, 1))) {
-                    orderStatus = "Dispatched";
-                } else if (today.before(addDays(orderDate, 2))) {
-                    orderStatus = "In Transit";
-                } else if (today.before(addDays(orderDate, 3))) {
-                    orderStatus = "In Transit";
-                } else if (today.before(addDays(orderDate, 4))) {
-                    orderStatus = "Shipped";
-                } else if (today.equals(deliveryDate)) {
-                    orderStatus = "Out for Delivery";
-                } else {
-                    orderStatus = "Delivered";
-                }
-
-                rowData.put("Order Status", orderStatus);
-
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public boolean isSameDay(Date date1, Date date2) {
-        Calendar cal1 = Calendar.getInstance();
-        cal1.setTime(date1);
-        Calendar cal2 = Calendar.getInstance();
-        cal2.setTime(date2);
-        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
-                cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH) &&
-                cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH);
-    }
-
-    // Helper method to add days to a Date
-    public Date addDays(Date date, int days) {
-        long timeInMillis = date.getTime();
-        timeInMillis += days * 24 * 60 * 60 * 1000; // Convert days to milliseconds
-        return new Date(timeInMillis);
-    }
-    public String generateRandom4Digit() {
-        Random random = new Random();
-        int random4DigitNumber = 1000 + random.nextInt(9000);
-        Date currentDate = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyy");
-        String formattedDate = dateFormat.format(currentDate);
-        return random4DigitNumber + formattedDate;
-    }
-
-
-    @Scheduled(cron = "0 0 10 * * *") // Every day at 10AM
-    public void updateOrderStatus() {
-        System.out.println("Cron Start");
-        try {
-            // Fetch data directly from Google Sheets
-            ValueRange response = sheets.spreadsheets().values().get(SpreadSheetId, "Data!A2:J").execute();
-            List<List<Object>> values = response.getValues();
-
-            // Skip if values is null or empty
-            if (values == null || values.isEmpty()) {
-                return;
-            }
-
-            // Update order status for each row
-            for (List<Object> row : values) {
-                String orderId = (String) row.get(0); // Assuming OrderId is in the first column (column A)
-
-                if (orderId != null && !orderId.isEmpty()) {
-                    String orderDateStr = (String) row.get(7);
-                    String deliveryDateStr = (String) row.get(8);
-
-                    try {
-                        Date orderDate = new SimpleDateFormat("dd-MM-yyyy").parse(orderDateStr);
-                        Date deliveryDate = new SimpleDateFormat("dd-MM-yyyy").parse(deliveryDateStr);
-
-                        String orderStatus = calculateOrderStatus(orderDate, deliveryDate);
-
-                        // Order Status column is in the last column (column J)
-                        List<List<Object>> updateValues = List.of(List.of(orderStatus));
-                        ValueRange body = new ValueRange().setValues(updateValues);
-
-                        sheets.spreadsheets().values()
-                                .update(SpreadSheetId, "Data!J" + (values.indexOf(row) + 2), body)
-                                .setValueInputOption("RAW")
-                                .execute();
-
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Cron end");
-    }
-
-    private String calculateOrderStatus(Date orderDate, Date deliveryDate) {
-        Date today = new Date();
-
-        if (isSameDay(today, orderDate)) {
-            return "Preparing for Dispatch";
-        } else if (today.before(addDays(orderDate, 1))) {
-            return "Dispatched";
-        } else if (today.before(addDays(orderDate, 2)) || today.before(addDays(orderDate, 3))) {
-            return "In Transit";
-        } else if (today.before(addDays(orderDate, 4))) {
-            return "Shipped";
-        } else if (today.equals(deliveryDate)) {
-            return "Out for Delivery";
-        } else {
-            return "Delivered";
-        }
-    }
-
     public List<Map<String, Object>> findRowsByNameAndNumber(
             List<List<Object>> allData, String name, String phoneNumber, List<String> columnOrder) {
         List<Map<String, Object>> result = new ArrayList<>();
@@ -218,7 +84,6 @@ public class SheetsService {
 
         return result;
     }
-
     public Map<String, Object> findRowByOrderId(List<List<Object>> allData, String orderId, List<String> columnOrder) {
         Map<String, Object> result = new LinkedHashMap<>();
 
@@ -238,6 +103,218 @@ public class SheetsService {
 
         return result;
     }
+    public Map<String, Object> createJsonResponseForWrite(int responseCode, List<Object> rowData) {
+        Map<String, Object> dataMap = new LinkedHashMap<>();
+        dataMap.put("OrderId", rowData.get(0));
+        dataMap.put("Name", rowData.get(1));
+        dataMap.put("PhoneNumber", rowData.get(2));
+        dataMap.put("ProductName", rowData.get(3));
+        dataMap.put("Units", rowData.get(4));
+        dataMap.put("Quantity", rowData.get(5));
+        dataMap.put("Price", rowData.get(6));
+        dataMap.put("OrderedDate", rowData.get(7));
+        dataMap.put("EstDeliveryDate", rowData.get(8));
+        dataMap.put("OrderStatus", rowData.get(9));
+
+        Map<String, Object> jsonResponse = new LinkedHashMap<>();
+        jsonResponse.put("responseCode", responseCode);
+        jsonResponse.put("data", dataMap);
+
+        return jsonResponse;
+    }
+
+    public String generateRandom4Digit() {
+        Random random = new Random();
+        int random4DigitNumber = 1000 + random.nextInt(9000);
+        Date currentDate = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyy");
+        String formattedDate = dateFormat.format(currentDate);
+        return random4DigitNumber + formattedDate;
+    }
+
+    @Scheduled(cron = "0 0/15 * * * *") // Every 15 minutes
+    public void updateOrderStatus() {
+        System.out.println("Cron Start");
+        try {
+            // Fetch data directly from Google Sheets
+            ValueRange response = sheets.spreadsheets().values().get(SpreadSheetId, "Data!A2:J").execute();
+            List<List<Object>> values = response.getValues();
+
+            // Skip if values is null or empty
+            if (values == null || values.isEmpty()) {
+                return;
+            }
+
+            // Update order status for each row
+            for (List<Object> row : values) {
+                // Extract order data
+                String orderId = (String) row.get(0);
+                String orderStatus = (String) row.get(9); //Order Status is in the 10th column (column J)
+
+                // Update order status based on logic
+                String newOrderStatus = getNextOrderStatus(orderStatus);
+
+                // Update the sheet with the new order status
+                List<List<Object>> updateValues = List.of(List.of(newOrderStatus));
+                ValueRange body = new ValueRange().setValues(updateValues);
+
+                sheets.spreadsheets().values()
+                        .update(SpreadSheetId, "Data!J" + (values.indexOf(row) + 2), body)
+                        .setValueInputOption("RAW")
+                        .execute();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Cron End");
+    }
+
+    private String getNextOrderStatus(String currentOrderStatus) {
+        if ("Preparing for Dispatch".equalsIgnoreCase(currentOrderStatus)) {
+            return "Dispatched";
+        } else if ("Dispatched".equalsIgnoreCase(currentOrderStatus)) {
+            return "In Transit";
+        } else if ("In Transit".equalsIgnoreCase(currentOrderStatus)) {
+            return "Shipped";
+        } else if ("Shipped".equalsIgnoreCase(currentOrderStatus)) {
+            return "Out for Delivery";
+        } else if ("Out for Delivery".equalsIgnoreCase(currentOrderStatus)) {
+            return "Delivered";
+        } else {
+            return currentOrderStatus; // No change if unknown status
+        }
+    }
+
+//    public void addOrderStatus(Map<String, Object> rowData) {
+//        String orderDateStr = (String) rowData.get("Ordered Date");
+//        String deliveryDateStr = (String) rowData.get("Est. Delivery Date");
+//
+//        if (orderDateStr != null && deliveryDateStr != null) {
+//            try {
+//                // Parse date strings into Date objects
+//                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+//                Date orderDate = dateFormat.parse(orderDateStr);
+//                Date deliveryDate = dateFormat.parse(deliveryDateStr);
+//
+//                // Get today's date
+//                Date today = dateFormat.parse(dateFormat.format(new Date())); // dd-MM-yyyy
+//
+//                // Determine the order status based on the logic
+//                String orderStatus = "";
+//
+//                if (isSameDay(today, orderDate)) {
+//                    orderStatus = "Preparing for Dispatch";
+//                } else if (today.before(addDays(orderDate, 1))) {
+//                    orderStatus = "Dispatched";
+//                } else if (today.before(addDays(orderDate, 2))) {
+//                    orderStatus = "In Transit";
+//                } else if (today.before(addDays(orderDate, 3))) {
+//                    orderStatus = "In Transit";
+//                } else if (today.before(addDays(orderDate, 4))) {
+//                    orderStatus = "Shipped";
+//                } else if (today.equals(deliveryDate)) {
+//                    orderStatus = "Out for Delivery";
+//                } else {
+//                    orderStatus = "Delivered";
+//                }
+//
+//                rowData.put("Order Status", orderStatus);
+//
+//            } catch (ParseException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+
+//    @Scheduled(cron = "0 0 10 * * *") // Every day at 10AM
+//    public void updateOrderStatus() {
+//        System.out.println("Cron Start");
+//        try {
+//            // Fetch data directly from Google Sheets
+//            ValueRange response = sheets.spreadsheets().values().get(SpreadSheetId, "Data!A2:J").execute();
+//            List<List<Object>> values = response.getValues();
+//
+//            // Skip if values is null or empty
+//            if (values == null || values.isEmpty()) {
+//                return;
+//            }
+//
+//            // Update order status for each row
+//            for (List<Object> row : values) {
+//                String orderId = (String) row.get(0); // Assuming OrderId is in the first column (column A)
+//
+//                if (orderId != null && !orderId.isEmpty()) {
+//                    String orderDateStr = (String) row.get(7);
+//                    String deliveryDateStr = (String) row.get(8);
+//
+//                    try {
+//                        Date orderDate = new SimpleDateFormat("dd-MM-yyyy").parse(orderDateStr);
+//                        Date deliveryDate = new SimpleDateFormat("dd-MM-yyyy").parse(deliveryDateStr);
+//
+//                        String orderStatus = calculateOrderStatus(orderDate, deliveryDate);
+//
+//                        // Order Status column is in the last column (column J)
+//                        List<List<Object>> updateValues = List.of(List.of(orderStatus));
+//                        ValueRange body = new ValueRange().setValues(updateValues);
+//
+//                        sheets.spreadsheets().values()
+//                                .update(SpreadSheetId, "Data!J" + (values.indexOf(row) + 2), body)
+//                                .setValueInputOption("RAW")
+//                                .execute();
+//
+//                    } catch (ParseException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                }
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("Cron end");
+//    }
+
+
+//    private String calculateOrderStatus(Date orderDate, Date deliveryDate) throws ParseException {
+//
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+//
+//        Date today = dateFormat.parse(dateFormat.format(new Date()));
+//
+//        if (isSameDay(today, orderDate)) {
+//            return "Preparing for Dispatch";
+//        } else if (today.before(addDays(orderDate, 1))) {
+//            return "Dispatched";
+//        } else if (today.before(addDays(orderDate, 2)) || today.before(addDays(orderDate, 3))) {
+//            return "In Transit";
+//        } else if (today.before(addDays(orderDate, 4))) {
+//            return "Shipped";
+//        } else if (today.equals(deliveryDate)) {
+//            return "Out for Delivery";
+//        } else {
+//            return "Delivered";
+//        }
+//    }
+//
+//    public boolean isSameDay(Date date1, Date date2) {
+//        Calendar cal1 = Calendar.getInstance();
+//        cal1.setTime(date1);
+//        Calendar cal2 = Calendar.getInstance();
+//        cal2.setTime(date2);
+//        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+//                cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH) &&
+//                cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH);
+//    }
+//
+//    // Helper method to add days to a Date
+//    public Date addDays(Date date, int days) {
+//        long timeInMillis = date.getTime();
+//        timeInMillis += days * 24 * 60 * 60 * 1000; // Convert days to milliseconds
+//        return new Date(timeInMillis);
+//    }
 
     public void writeData(String spreadsheetId, String range, SheetsData sheetData) throws IOException {
         List<Object> rowData = Arrays.asList(
@@ -278,25 +355,5 @@ public class SheetsService {
         }
 
         return result;
-    }
-
-    public Map<String, Object> createJsonResponseForWrite(int responseCode, List<Object> rowData) {
-        Map<String, Object> dataMap = new LinkedHashMap<>();
-        dataMap.put("OrderId", rowData.get(0));
-        dataMap.put("Name", rowData.get(1));
-        dataMap.put("PhoneNumber", rowData.get(2));
-        dataMap.put("ProductName", rowData.get(3));
-        dataMap.put("Units", rowData.get(4));
-        dataMap.put("Quantity", rowData.get(5));
-        dataMap.put("Price", rowData.get(6));
-        dataMap.put("OrderedDate", rowData.get(7));
-        dataMap.put("EstDeliveryDate", rowData.get(8));
-        dataMap.put("OrderStatus", rowData.get(9));
-
-        Map<String, Object> jsonResponse = new LinkedHashMap<>();
-        jsonResponse.put("responseCode", responseCode);
-        jsonResponse.put("data", dataMap);
-
-        return jsonResponse;
     }
 }
